bf49e5395ea79912889bd5a782ed5bd5ba271c00
bahar <kiani.bahare11@gmail.com>
2026-07-13 11:12:44 +0330

    Initial commit

---
+++ src/main/java/com/example/TicketSupport/TicketSupportApplication.java
@@ -0,0 +1,14 @@
+package com.example.TicketSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class TicketSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TicketSupport/TicketSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicketSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
