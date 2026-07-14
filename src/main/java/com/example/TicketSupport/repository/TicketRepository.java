2678fd563237440eaeb1f479bc1beb1ce05fee58
bahar <kiani.bahare11@gmail.com>
2026-07-14 09:23:09 +0330

    refactor: correct generic type usage

--- src/main/java/com/example/TicketSupport/repository/TicketRepository.java
+++ src/main/java/com/example/TicketSupport/repository/TicketRepository.java
@@ -1,9 +1,10 @@
 package com.example.TicketSupport.repository;
 
+import com.example.TicketSupport.entity.Ticket;
 import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Registered
-public interface TicketRepository extends JpaRepository<TicketRepository,Long> {
+public interface TicketRepository extends JpaRepository<Ticket,Long> {
 
 }
