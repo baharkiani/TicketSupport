3488efa6d015362dd34808b1a708a99fcabadf86
bahar <kiani.bahare11@gmail.com>
2026-08-19 11:46:13 +0330

    feat: add refreshToken repository

---
+++ src/main/java/com/example/TicketSupport/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.example.TicketSupport.repository;
+
+import com.example.TicketSupport.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByToken(String token);
+
+}
