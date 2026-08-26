0985c08477507a0a75096004b824ab07ec15e15d
bahar <kiani.bahare11@gmail.com>
2026-08-26 09:59:14 +0330

    feat(auth): secure user getAll endpoint with JWT and admin role

--- src/main/java/com/example/TicketSupport/controller/AuthController.java
+++ src/main/java/com/example/TicketSupport/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.example.TicketSupport.controller;
 
+import com.example.TicketSupport.annotation.JwtRequired;
 import com.example.TicketSupport.dto.*;
-import com.example.TicketSupport.entity.RefreshToken;
 import com.example.TicketSupport.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/api")
@@ -33,6 +36,14 @@ public class AuthController {
     @PostMapping("/refresh")
     public ResponseEntity<String> refresh(@Valid @RequestBody RefreshTokenRequest request){
         return ResponseEntity.ok(authService.refreshAccessToken(request.getRefreshToken()));
+    }
+
+    @JwtRequired
+    @SecurityRequirement(name = "bearerAuth")
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<UserResponse>> getAll(Pageable pageable) {
+        return ResponseEntity.ok((Page<UserResponse>) authService.getAll(pageable));
     }
 
 }
