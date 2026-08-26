22828e0f526e48621fa18ad81f4796ff0f4db0dd
bahar <kiani.bahare11@gmail.com>
2026-08-26 10:32:57 +0330

    feat : add Permission Entity

---
+++ src/main/java/com/example/TicketSupport/entity/Permission.java
@@ -0,0 +1,34 @@
+package com.example.TicketSupport.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "permissions")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
